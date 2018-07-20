54a5d8d95d4ca44ce523ab86106c0fb2a8d4cbe6
pratiknaik-coditas <pratik.naik@coditas.com>
2018-07-20 19:51:54 +0530

    success

--- src/component/Card.js
+++ src/component/Card.js
@@ -2,28 +2,44 @@
 
 export class Card extends React.Component{
 
-  render(){
+    constructor(){
+      super();
+      this.renderComponents=this.renderComponents.bind(this); 
 
-    console.log(JSON.stringify(this.props.users));
+    }
+
+
+  renderComponents(obj)
+  {
 
     return(
-        <div className="shadow p-4 mb-4 bg-white">
-          <div className="row">
-              <div className="col-md-4">
-                <img className="rounded"/>
-              </div>
+      <div className="shadow p-4 mb-4 bg-white">
+        <div className="row">
+            <div className="col-md-4">
+              <img className="rounded"/>
+            </div>
 
-            <div className="col-md-6">
-              <h3></h3><br/>
-              <label>Profile</label><br/>
-              <label>Data one:</label><br/>
-              <label>Data Two:</label><br/>
-            </div>
-            <div className="col-md-2">
-            </div>
-          </div>              
-        </div>
-        );
+          <div className="col-md-6">
+            <h3></h3><br/>
+            <label>Profile:&nbsp; </label>{obj.login}<br/>
+            <label>Data one:</label><br/>
+            <label>Data Two:</label><br/>
+          </div>
+          <div className="col-md-2">
+          </div>
+        </div>              
+      </div>
+      );
+
+
+  }
+
+  render(){
+
+    //console.log(typeof this.props.users);
+    console.log(  );
+    return(this.props.users.map(this.renderComponents));
+    
   }
 }
 
